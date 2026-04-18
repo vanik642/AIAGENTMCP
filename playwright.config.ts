62e95b946f2b045cac5b5d076563eb1f1f4524d7
Vanita K <vkanaje@gmail.com>
2026-04-18 14:09:11 -0700

    Update playwright config: enable headless mode, set viewport to null, and add maximized window launch options

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   use: {
     baseURL: 'http://127.0.0.1:3000',
     trace: 'on-first-retry',
-    headless:false,
+    headless:true,
     viewport: null,
   launchOptions: {
     args: ['--start-maximized']
